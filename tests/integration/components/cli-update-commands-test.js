779514ac8fc01db1f58929d048266b32b27e54c5
Abhilash <abhilashlr.789@gmail.com>
2020-04-12 19:48:26 +0530

    #6 (feat): ember-cli-update command

---
+++ tests/integration/components/cli-update-commands-test.js
@@ -0,0 +1,23 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | cli-update-commands', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it checks if the right command is displayed', async function(assert) {
+    this.versions = [
+      3.13,
+      3.14,
+      3.15,
+      3.16
+    ];
+    this.toVersion = 3.14;
+
+    await render(hbs`<CliUpdateCommands @toVersion={{this.toVersion}} @versions={{this.versions}} />`);
+
+    assert.equal(this.element.querySelectorAll('.es-codesample')[0].textContent.trim(), 'ember-cli-update --to 3.14');
+    assert.equal(this.element.querySelectorAll('.es-codesample')[1].textContent.trim(), 'ember-cli-update --run-codemods');
+  });
+});
