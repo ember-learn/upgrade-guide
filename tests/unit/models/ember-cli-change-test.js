e671881b3a6514bcaceef78a4617c3391d965ec9
Abhilash L R <abhilashlr.789@gmail.com>
2020-05-02 08:26:36 +0530

    Change the unit test file

--- tests/unit/models/ember-cli-change-test.js
+++ tests/unit/models/ember-cli-change-test.js
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Model | ember cli', function(hooks) {
+module('Unit | Model | ember cli change', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
     let store = this.owner.lookup('service:store');
-    let model = store.createRecord('ember-cli', {});
+    let model = store.createRecord('ember-cli-change', {});
 
     assert.ok(model);
   });
