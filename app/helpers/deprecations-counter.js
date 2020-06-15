09a493f094c9e154bb21723dab96fe8b87b69694
JennyJudova <jjudova@gmail.com>
2020-06-15 19:31:18 +0100

    fix lint errors

--- app/helpers/deprecations-counter.js
+++ app/helpers/deprecations-counter.js
@@ -4,6 +4,10 @@ export default helper(function counter(params) {
   let models = params[0].content;
   let toVersion = params[1];
   let fromVersion = params[2];
-  let depArr = models.filter((model) => model.id > fromVersion && model.id <= toVersion).map(model => model.__recordData.__data.changes).flatMap((array) => array).filter(object => object.deprecation);
+  let depArr = models
+    .filter((model) => model.id > fromVersion && model.id <= toVersion)
+    .map((model) => model.__recordData.__data.changes)
+    .flatMap((array) => array)
+    .filter((object) => object.deprecation);
   return depArr.length;
 });
--- app/helpers/features-counter.js
+++ app/helpers/features-counter.js
@@ -4,6 +4,10 @@ export default helper(function counter(params) {
   let models = params[0].content;
   let toVersion = params[1];
   let fromVersion = params[2];
-  let featArr = models.filter((model) => model.id > fromVersion && model.id <= toVersion).map(model => model.__recordData.__data.changes).flatMap((array) => array).filter(object => object.feature);
+  let featArr = models
+    .filter((model) => model.id > fromVersion && model.id <= toVersion)
+    .map((model) => model.__recordData.__data.changes)
+    .flatMap((array) => array)
+    .filter((object) => object.feature);
   return featArr.length;
 });
