63d4c93ef736cc05d211f64d65a37b0dcb003129
ijlee2 <ijlee2@users.noreply.github.com>
2020-09-23 08:00:49 -0500

    Added Ember v3.21 as a select option

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,11 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 import { VERSIONS } from '../models/versions';
 
 export default class IndexController extends Controller {
   queryParams = ['fromVersion', 'toVersion'];
-  fromVersion = '3.15';
-  toVersion = '3.20';
+
   versions = VERSIONS;
+  @tracked fromVersion = '3.15';
+  @tracked toVersion = VERSIONS[VERSIONS.length - 1];
 }
