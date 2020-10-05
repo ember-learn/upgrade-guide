115fabe9d79fb2dae1780b13b84597cd68aa41d1
Jayvic San Antonio <20731321+jpsanantonio@users.noreply.github.com>
2020-10-04 21:21:41 -0700

    Componentize the Ember Versions Form
    Add spacing

---
+++ app/components/ember-versions-form.js
@@ -0,0 +1,33 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { VERSIONS } from '../models/versions';
+
+export default class EmberVersionsFormComponent extends Component {
+  versions = VERSIONS;
+  @tracked fromVersion = '3.15';
+  @tracked toVersion = VERSIONS[VERSIONS.length - 1];
+
+  @action submitForm(event) {
+    event.preventDefault();
+
+    const { onSubmit } = this.args;
+
+    if (onSubmit) {
+      onSubmit({
+        fromVersion: this.fromVersion,
+        toVersion: this.toVersion,
+      });
+    }
+  }
+
+  @action updateFromVersion(event) {
+    this.fromVersion = event.target.value;
+    this.submitForm(event);
+  }
+
+  @action updateToVersion(event) {
+    this.toVersion = event.target.value;
+    this.submitForm(event);
+  }
+}
