5b8f41ad0a84ee251c40146bae0c09d2bc12dcb2
Jayvic San Antonio <sanantonio.jayvic@gmail.com>
2020-10-05 20:53:46 -0700

    Added the displayChanges action to index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { VERSIONS } from 'upgrade-guide/utils/ember-versions';
 
@@ -8,4 +9,9 @@ export default class IndexController extends Controller {
   versions = VERSIONS;
   @tracked fromVersion = '3.15';
   @tracked toVersion = VERSIONS[VERSIONS.length - 1];
+
+  @action displayChanges({ fromVersion, toVersion }) {
+    this.fromVersion = fromVersion;
+    this.toVersion = toVersion;
+  }
 }
