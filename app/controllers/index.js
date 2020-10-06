a1c661dbf999614ae207481c7c1ae32ebdfd9853
Jayvic San Antonio <sanantonio.jayvic@gmail.com>
2020-10-05 20:47:05 -0700

    Removed doNotSubmitForm, updateFromVersion, and updateToVersion from index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,4 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { VERSIONS } from 'upgrade-guide/utils/ember-versions';
 
@@ -9,16 +8,4 @@ export default class IndexController extends Controller {
   versions = VERSIONS;
   @tracked fromVersion = '3.15';
   @tracked toVersion = VERSIONS[VERSIONS.length - 1];
-
-  @action doNotSubmitForm(event) {
-    event.preventDefault();
-  }
-
-  @action updateFromVersion(event) {
-    this.fromVersion = event.target.value;
-  }
-
-  @action updateToVersion(event) {
-    this.toVersion = event.target.value;
-  }
 }
