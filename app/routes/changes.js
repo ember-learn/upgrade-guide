879fb870e8961489b4b53126eebe6b5e9c145953
Jayvic San Antonio <sanantonio.jayvic@gmail.com>
2020-10-11 21:17:15 -0700

    Copy-pasted the model hook from the index route to changes route

--- app/routes/changes.js
+++ app/routes/changes.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
-
+import { hash } from 'rsvp';
 export default class ChangesRoute extends Route {
+  model() {
+    return hash({
+      emberJSChanges: this.store.findAll('ember-js-change'),
+      emberCLIChanges: this.store.findAll('ember-cli-change'),
+      emberDataChanges: this.store.findAll('ember-data-change'),
+    });
+  }
 }
