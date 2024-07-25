08d6d0e5144665c116ae775c7103ee5c81f94616
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2024-07-25 14:53:36 +0200

    Fix deps update

--- app/routes/changes.js
+++ app/routes/changes.js
@@ -1,6 +1,10 @@
 import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
+import { inject as service } from '@ember/service';
+
 export default class ChangesRoute extends Route {
+  @service store;
+
   model() {
     return hash({
       emberJSChanges: this.store.findAll('ember-js-change'),
