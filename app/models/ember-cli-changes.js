a38e98cdc3859c8389e943b3dcfd0dbba1534af7
Tom Carter <tommyjr@gmail.com>
2026-08-06 10:37:35 -0500

    Remove model inheritence (schema prep)

--- app/models/ember-cli-changes.js
+++ app/models/ember-cli-changes.js
@@ -1,3 +1,20 @@
-import CommonModel from './private/-common';
+import Model, { attr } from '@warp-drive/legacy/model';
 
-export default class EmberCliChangesModel extends CommonModel {}
+export default class EmberCliChangesModel extends Model {
+  @attr version;
+  @attr changes;
+
+  // html and content are not used, but they appear in the data
+  @attr html;
+  @attr content;
+
+  get features() {
+    return (this.changes || []).filter((change) => Boolean(change?.feature));
+  }
+
+  get deprecations() {
+    return (this.changes || []).filter((change) =>
+      Boolean(change?.deprecation),
+    );
+  }
+}
--- app/models/ember-js-changes.js
+++ app/models/ember-js-changes.js
@@ -1,3 +1,20 @@
-import CommonModel from './private/-common';
+import Model, { attr } from '@warp-drive/legacy/model';
 
-export default class EmberJsChangesModel extends CommonModel {}
+export default class EmberJsChangesModel extends Model {
+  @attr version;
+  @attr changes;
+
+  // html and content are not used, but they appear in the data
+  @attr html;
+  @attr content;
+
+  get features() {
+    return (this.changes || []).filter((change) => Boolean(change?.feature));
+  }
+
+  get deprecations() {
+    return (this.changes || []).filter((change) =>
+      Boolean(change?.deprecation),
+    );
+  }
+}
